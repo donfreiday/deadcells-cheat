8f1871524eade16104adbbc374b6bd3030fa64d7
Don <donfreiday@gmail.com>
2018-11-21 17:49:41 -0500

    Change path

--- savestate.sh
+++ savestate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mpv ~/projects/deadcells-cheat/Ching2.wav &
-rm -rf ~/Dead\ Cells/savestate
-cp -r ~/Dead\ Cells/save ~/Dead\ Cells/savestate
+rm -rf ~/Dead\ Cells/game/savestate
+cp -r ~/Dead\ Cells/game/save ~/Dead\ Cells/game/savestate
