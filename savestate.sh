1b40c95c928c59aedbb6bf7d83164111f2260972
Don <donfreiday@gmail.com>
2018-12-01 12:21:06 -0500

    Backup save to Dropbox on savestate

--- savestate.sh
+++ savestate.sh
@@ -2,3 +2,4 @@
 mpv ~/projects/deadcells-cheat/Ching2.wav &
 rm -rf ~/Dead\ Cells/game/savestate
 cp -r ~/Dead\ Cells/game/save ~/Dead\ Cells/game/savestate
+cp -r ~/Dead\ Cells/game/save ~/Dropbox/saves/Dead\ Cells/save
